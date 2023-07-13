f2c8761a2b50dd41e450c8c0a503d9ed9c8dcae4
Chris Simmons <chris@turing.edu>
2023-07-12 20:17:06 -0400

    Adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+shelter = Shelter.create(name: "Mystery Building", city: "Irvine CA", foster_program: false, rank: 9)
+shelter2 = Shelter.create(name: "Chris' House", city: "Columbus OH", foster_program: false, rank: 9)
+pet = Pet.create(name: "Scooby", age: 5, breed: "Great Dane", adoptable: true, shelter_id: shelter.id)
+Pet.create!(name: "Shaggy", age: 1, breed: "Great Dane", adoptable: true, shelter: shelter)
+Pet.create!(name: "Charlie", age: 3, breed: "Domestic longhair", adoptable: false, shelter: shelter2)
+Pet.create!(name: "Zoey", age: 3, breed: "Domestic longhair", adoptable: false, shelter: shelter2)
+Pet.create!(name: "Momo", age: 4, breed: "Domestic longhair", adoptable: false, shelter: shelter2)
